ccf68b1bf84a6f2f8013eefa05fbd1334f69c939
Nicholas Marriott <nicholas.marriott@neueda.com>
2014-12-01 12:44:54 +0000

    Sync.

--- common/sbfCommon.h
+++ common/sbfCommon.h
@@ -14,17 +14,21 @@
 #ifdef linux
 #include "sbfCommonLinux.h"
 #endif
-
 #ifdef WIN32
 #include "sbfCommonWin32.h"
 #endif
+#ifdef __APPLE__
+#include "sbfCommonDarwin.h"
+#endif
+
+#include <event.h>
 
 #include "syshash.h"
 #include "sysqueue.h"
 #include "systree.h"
 
-#include "sbfLog.h"
 #include "sbfFatal.h"
+#include "sbfLog.h"
 #include "sbfMemory.h"
 
 SBF_BEGIN_DECLS
@@ -32,7 +36,55 @@ SBF_BEGIN_DECLS
 #define SBF_MIN(a, b) ((a) > (b) ? (b) : (a))
 #define SBF_MAX(a, b) ((a) < (b) ? (b) : (a))
 
+#define SBF_PAD(size, to) (((size) + ((to) - 1)) & ~((to) - 1))
+
+#define SBF_ARRAYSIZE(a) (sizeof (a) / sizeof ((a)[0]))
+
 typedef int sbfError; /* errno return */
+
+#define SBF_RB_TREE(name, suffix, entry, compare)           \
+    RB_HEAD (name ## suffix ## Impl, name ## Impl);         \
+    typedef struct name ## suffix ## Impl name ## suffix;   \
+                                                            \
+    static SBF_INLINE int                                   \
+    name ## suffix ## Cmp (name lhs, name rhs)              \
+    compare                                                 \
+    RB_GENERATE_STATIC (name ## suffix ## Impl,             \
+                        name ## Impl,                       \
+                        entry,                              \
+                        name ## suffix ## Cmp)
+
+static SBF_INLINE uint64_t
+sbf_htonll (uint64_t val)
+{
+    uint32_t b;
+    uint32_t t;
+
+    b = htonl (val & 0xffffffff);
+    t = htonl (val >> 32);
+
+    return ((uint64_t)b << 32 | t);
+}
+
+#ifndef htonll
+#define htonll sbf_htonll
+#endif
+
+static SBF_INLINE uint64_t
+sbf_ntohll (uint64_t val)
+{
+    uint32_t b;
+    uint32_t t;
+
+    b = ntohl (val & 0xffffffff);
+    t = ntohl (val >> 32);
+
+    return ((uint64_t)b << 32 | t);
+}
+
+#ifndef ntohll
+#define ntohll sbf_ntohll
+#endif
 
 SBF_END_DECLS
 
