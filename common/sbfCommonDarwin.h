803a114d3aaeee221efcc51ef442238dc4bf6032
Philip Herron <herron.philip@googlemail.com>
2018-03-29 21:55:45 +0100

    Fix build on macosx

--- common/sbfCommonDarwin.h
+++ common/sbfCommonDarwin.h
@@ -42,6 +42,7 @@
 #include <time.h>
 #include <unistd.h>
 
+#include <os/lock.h>
 #include <libkern/OSAtomic.h>
 
 SBF_BEGIN_DECLS
@@ -89,11 +90,11 @@ int sbfMutex_init (pthread_mutex_t* m, int recursive);
 #define sbfMutex_lock(m) pthread_mutex_lock (m)
 #define sbfMutex_unlock(m) pthread_mutex_unlock (m)
 
-typedef OSSpinLock sbfSpinLock;
+typedef os_unfair_lock_t sbfSpinLock;
 #define sbfSpinLock_init(s) do { *s = 0; } while (0)
 #define sbfSpinLock_destroy(s)
-#define sbfSpinLock_lock(s) OSSpinLockLock (s)
-#define sbfSpinLock_unlock(s) OSSpinLockUnlock (s)
+#define sbfSpinLock_lock(s) os_unfair_lock_lock (*(s))
+#define sbfSpinLock_unlock(s) os_unfair_lock_unlock (*(s))
 
 SBF_END_DECLS
 
