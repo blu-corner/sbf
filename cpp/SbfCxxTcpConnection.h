5cb53602dd5fe136b1ed4ca9ced2b14fa43aa96c
Philip Herron <phil.herron@neueda.com>
2018-04-23 13:30:51 +0100

    make header only implementation

--- cpp/SbfCxxTcpConnection.h
+++ cpp/SbfCxxTcpConnection.h
@@ -22,30 +22,100 @@ public:
                       struct sbfQueueImpl* queue,
                       sbfTcpConnectionAddress* address,
                       bool isUnix,
-                      SbfTcpConnectionDelegate* delegate);
+                      SbfTcpConnectionDelegate* delegate)
+        : mConnection (NULL),
+          mLog (log),
+          mThread (thread),
+          mQueue (queue),
+          mAddress (address),
+          mIsUnix (isUnix),
+          mDelegate (delegate)
+    {
+    }
 
-    virtual ~SbfTcpConnection ();
+    virtual ~SbfTcpConnection ()
+    {
+        if (getHandle () != NULL)
+            sbfTcpConnection_destroy (getHandle ());
+    }
 
-    virtual sbfTcpConnection getHandle ();
+    virtual sbfTcpConnection getHandle ()
+    {
+        return mConnection;
+    }
 
-    virtual void send (const void* data, size_t size);
+    virtual bool connect ()
+    {
+        mConnection = sbfTcpConnection_create (mLog,
+                                               mThread,
+                                               mQueue,
+                                               mAddress,
+                                               mIsUnix ? 1 : 0,
+                                               SbfTcpConnection::sbfTcpConnectionReadyCb,
+                                               SbfTcpConnection::sbfTcpConnectionErrorCb,
+                                               SbfTcpConnection::sbfTcpConnectionReadCb,
+                                               this);
+        return mConnection != NULL;
+    }
 
-    virtual void sendBuffer (sbfBuffer buffer);
+    virtual void send (const void* data, size_t size)
+    {
+        sbfTcpConnection_send (getHandle (), data, size);
+    }
 
-    virtual const sbfTcpConnectionAddress* getPeer ();
+    virtual void sendBuffer (sbfBuffer buffer)
+    {
+        sbfTcpConnection_sendBuffer (getHandle (), buffer);
+    }
 
-    virtual bool isUnix ();
+    virtual const sbfTcpConnectionAddress* getPeer ()
+    {
+        return sbfTcpConnection_getPeer (getHandle ());
+    }
+
+    virtual bool isUnix ()
+    {
+        return sbfTcpConnection_isUnix (getHandle ());
+    }
 
 protected:
-    sbfTcpConnection mConnection;
+    sbfTcpConnection          mConnection;
+    sbfLog                    mLog;
+    struct sbfMwThreadImpl*   mThread;
+    struct sbfQueueImpl*      mQueue;
+    sbfTcpConnectionAddress*  mAddress;
+    bool                      mIsUnix;
     SbfTcpConnectionDelegate* mDelegate;
     
 private:
-    static void sbfTcpConnectionReadyCb (sbfTcpConnection tc, void* closure);
-    static void sbfTcpConnectionErrorCb (sbfTcpConnection tc, void* closure);
+    static void sbfTcpConnectionReadyCb (sbfTcpConnection tc, void* closure)
+    {
+        SbfTcpConnection* instance = static_cast<SbfTcpConnection*>(closure);
+        if (instance->mDelegate)
+        {
+            instance->mDelegate->onReady ();
+        }
+    }
+    
+    static void sbfTcpConnectionErrorCb (sbfTcpConnection tc, void* closure)
+    {
+        SbfTcpConnection* instance = static_cast<SbfTcpConnection*>(closure);
+        if (instance->mDelegate)
+        {
+            instance->mDelegate->onError ();
+        }
+    }
+    
     static size_t sbfTcpConnectionReadCb (sbfTcpConnection tc,
                                           void* data,
                                           size_t size,
-                                          void* closure);
+                                          void* closure)
+    {
+        SbfTcpConnection* instance = static_cast<SbfTcpConnection*>(closure);
+        if (instance->mDelegate)
+        {
+            return instance->mDelegate->onRead (data, size);
+        }
+    }
 };
 }
