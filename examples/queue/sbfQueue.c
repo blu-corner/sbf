4f93ec89df40cad5addf9147e513501f9f0cf386
Colin Pattison <colin.pattison@neueda.com>
2018-07-23 16:40:07 +0100

    Fix for Queue Destroy and Addition of C++ Wrapper
    - destroy event was being incorrectly queued
    - added c++ wrapper for some of the modules within the api

---
+++ examples/queue/sbfQueue.c
@@ -0,0 +1,49 @@
+/*!
+   \file sbfQueue.c
+   \brief TBD
+   \Copyright 2014-2018 Neueda Ltd.
+*/
+#include "sbfCommon.h"
+#include "sbfMw.h"
+#include "sbfEvent.h"
+
+static void*
+dispatchCb (void* closure)
+{
+    sbfQueue_dispatch (closure);
+    return NULL;
+}
+
+static void
+queueCb (sbfQueueItem item, void* closure)
+{
+    printf ("queue callback\n");
+}
+
+int
+main (int argc, char** argv)
+{
+    sbfLog      log;
+    sbfMw       mw;
+    sbfQueue    queue;
+    sbfThread   t;
+    sbfKeyValue properties;
+
+    log = sbfLog_create (NULL, "%s", "");
+    sbfLog_setLevel (log, SBF_LOG_DEBUG);
+
+    properties = sbfKeyValue_create ();
+    mw = sbfMw_create (log, properties);
+    queue = sbfQueue_create (mw, "default");
+
+    sbfQueue_enqueue (queue, queueCb, NULL);
+    sbfQueue_enqueue (queue, queueCb, NULL);
+    sbfQueue_enqueue (queue, queueCb, NULL);
+
+    sbfThread_create (&t, dispatchCb, queue);
+    
+    sbfQueue_destroy (queue);
+    sbfThread_join (t);
+
+    return 0;
+}
---
+++ src/cpp/SbfTcpConnection.hpp
@@ -0,0 +1,126 @@
+#pragma once
+
+#include "sbfTcpConnection.h"
+
+namespace neueda
+{
+class SbfTcpConnectionDelegate {
+public:
+    virtual ~SbfTcpConnectionDelegate () { }
+    
+    virtual void onReady () { }
+
+    virtual void onError () { }
+
+    virtual size_t onRead (void* data, size_t size) { return size; }
+};
+
+class SbfTcpConnection {
+public:
+    SbfTcpConnection (sbfLog log,
+                      struct sbfMwThreadImpl* thread,
+                      struct sbfQueueImpl* queue,
+                      sbfTcpConnectionAddress* address,
+                      bool isUnix,
+                      bool disableNagles,
+                      SbfTcpConnectionDelegate* delegate)
+        : mConnection (NULL),
+          mLog (log),
+          mThread (thread),
+          mQueue (queue),
+          mAddress (address),
+          mIsUnix (isUnix),
+          mDisableNagles (disableNagles),
+          mDelegate (delegate)
+    {
+    }
+
+    virtual ~SbfTcpConnection ()
+    {
+        if (getHandle () != NULL)
+            sbfTcpConnection_destroy (getHandle ());
+    }
+
+    virtual sbfTcpConnection getHandle ()
+    {
+        return mConnection;
+    }
+
+    virtual bool connect ()
+    {
+        mConnection = sbfTcpConnection_create (mLog,
+                                               mThread,
+                                               mQueue,
+                                               mAddress,
+                                               mIsUnix ? 1 : 0,
+                                               mDisableNagles ? 1 : 0,
+                                               SbfTcpConnection::sbfTcpConnectionReadyCb,
+                                               SbfTcpConnection::sbfTcpConnectionErrorCb,
+                                               SbfTcpConnection::sbfTcpConnectionReadCb,
+                                               this);
+        return mConnection != NULL;
+    }
+
+    virtual void send (const void* data, size_t size)
+    {
+        sbfTcpConnection_send (getHandle (), data, size);
+    }
+
+    virtual void sendBuffer (sbfBuffer buffer)
+    {
+        sbfTcpConnection_sendBuffer (getHandle (), buffer);
+    }
+
+    virtual const sbfTcpConnectionAddress* getPeer ()
+    {
+        return sbfTcpConnection_getPeer (getHandle ());
+    }
+
+    virtual bool isUnix ()
+    {
+        return sbfTcpConnection_isUnix (getHandle ());
+    }
+
+protected:
+    sbfTcpConnection          mConnection;
+    sbfLog                    mLog;
+    struct sbfMwThreadImpl*   mThread;
+    struct sbfQueueImpl*      mQueue;
+    sbfTcpConnectionAddress*  mAddress;
+    bool                      mIsUnix;
+    bool                      mDisableNagles;
+    SbfTcpConnectionDelegate* mDelegate;
+    
+private:
+    static void sbfTcpConnectionReadyCb (sbfTcpConnection tc, void* closure)
+    {
+        SbfTcpConnection* instance = static_cast<SbfTcpConnection*>(closure);
+        if (instance->mDelegate)
+        {
+            instance->mDelegate->onReady ();
+        }
+    }
+    
+    static void sbfTcpConnectionErrorCb (sbfTcpConnection tc, void* closure)
+    {
+        SbfTcpConnection* instance = static_cast<SbfTcpConnection*>(closure);
+        if (instance->mDelegate)
+        {
+            instance->mDelegate->onError ();
+        }
+    }
+    
+    static size_t sbfTcpConnectionReadCb (sbfTcpConnection tc,
+                                          void* data,
+                                          size_t size,
+                                          void* closure)
+    {
+        SbfTcpConnection* instance = static_cast<SbfTcpConnection*>(closure);
+        if (instance->mDelegate)
+        {
+            return instance->mDelegate->onRead (data, size);
+        }
+        return size;
+    }
+};
+}
